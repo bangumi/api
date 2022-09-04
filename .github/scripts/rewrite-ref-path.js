b8fbe1429600139593c601a3bcad5aed982cce15
Trim21 <trim21.me@gmail.com>
2022-09-05 02:11:57 +0800

    add legacy prefix to old api

--- .github/scripts/rewrite-ref-path.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-
-const apiDoc = fs.readFileSync('open-api/api.yml', {encoding: 'utf-8'});
-
-fs.writeFileSync('open-api/api-gen.yml', apiDoc.replaceAll('v0.yaml#/components/schemas', '#/components/schemas'));
-
